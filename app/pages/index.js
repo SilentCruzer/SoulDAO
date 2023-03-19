b3a24ea5860627095ad8d2db507e2b338cc156c0
SilentCruzer <radithyakumar7@gmail.com>
2023-03-19 21:34:29 +0530

    Formats index code

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,9 +1,16 @@
 import { useState, useEffect } from "react";
 import { ethers } from "ethers";
-import { membershipABI, membershipAddress, daoABI, daoAddress } from "@/constants";
+import {
+  membershipABI,
+  membershipAddress,
+  daoABI,
+  daoAddress,
+} from "@/constants";
 import { useRouter } from "next/router";
 
-const provider = new ethers.providers.JsonRpcProvider(process.env.NEXT_PUBLIC_RPC_URL);
+const provider = new ethers.providers.JsonRpcProvider(
+  process.env.NEXT_PUBLIC_RPC_URL
+);
 
 export default function MyComponent() {
   const [userAddress, setUserAddress] = useState("");
@@ -14,30 +21,36 @@ export default function MyComponent() {
   const [walletErrorMessage, setWalletErrorMessage] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
-  const memberContract = new ethers.Contract(membershipAddress, membershipABI, provider);
+  const memberContract = new ethers.Contract(
+    membershipAddress,
+    membershipABI,
+    provider
+  );
   const daoContract = new ethers.Contract(daoAddress, daoABI, provider);
   const router = useRouter();
 
   useEffect(() => {
     async function fetchData() {
-      
       if (typeof window.ethereum !== "undefined") {
         setIsLoading(true);
         try {
           await window.ethereum.request({ method: "eth_requestAccounts" });
-          
-          const accounts = await window.ethereum.request({ method: "eth_accounts" });
+
+          const accounts = await window.ethereum.request({
+            method: "eth_accounts",
+          });
           setUserAddress(accounts[0]);
           setIsWalletConnected(true);
 
           const tokenId = 1;
-          const tokenBalance = await memberContract.balanceOf(accounts[0], tokenId);
+          const tokenBalance = await memberContract.balanceOf(
+            accounts[0],
+            tokenId
+          );
           const result = await daoContract.members(accounts[0]);
 
-          if(result ==1)
-            setMappingResult(true);
-          else
-            setMappingResult(false);
+          if (result == 1) setMappingResult(true);
+          else setMappingResult(false);
 
           setHasToken(tokenBalance > 0);
           setMappingResult(result);
@@ -59,7 +72,9 @@ export default function MyComponent() {
       try {
         await window.ethereum.request({ method: "eth_requestAccounts" });
         setIsWalletConnected(true);
-        const accounts = await window.ethereum.request({ method: "eth_accounts" });
+        const accounts = await window.ethereum.request({
+          method: "eth_accounts",
+        });
         setUserAddress(accounts[0]);
       } catch (error) {
         setWalletErrorMessage(error.message);
@@ -80,7 +95,6 @@ export default function MyComponent() {
     const tx = await memberContract.connect(signer).mint(1);
     await tx.wait();
     alert("Token minted successfully");
-  
   }
 
   async function joinDAO() {
@@ -97,17 +111,21 @@ export default function MyComponent() {
     alert("Joined DAO successfully");
   }
 
-  if(isLoading){
-    return <div className="flex flex-col items-center justify-center h-screen">
-      <div>Loading......</div>
-    </div>
+  if (isLoading) {
+    return (
+      <div className="flex flex-col items-center justify-center h-screen">
+        <div>Loading......</div>
+      </div>
+    );
   }
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       {!isWalletConnected && (
         <>
-          <h1 className="text-2xl font-bold mb-4">Please connect your wallet to continue</h1>
+          <h1 className="text-2xl font-bold mb-4">
+            Please connect your wallet to continue
+          </h1>
           <div onClick={connectWallet}>Connect Wallet</div>
           {walletErrorMessage && (
             <p className="text-red-500 text-sm mt-2">{walletErrorMessage}</p>
@@ -131,14 +149,12 @@ export default function MyComponent() {
         <>
           <h1 className="text-2xl font-bold mb-4">Join our DAO!</h1>
           <button onClick={() => joinDAO()}>Join</button>
-          </>
+        </>
       )}
       {isWalletConnected && !hasToken && (
         <>
           <h1 className="text-2xl font-bold mb-4">Mint your ERC1155 token</h1>
-          <button onClick={() => mint()}>
-            Mint Token
-          </button>
+          <button onClick={() => mint()}>Mint Token</button>
         </>
       )}
     </div>
